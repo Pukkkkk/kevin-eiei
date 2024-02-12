67af8a14ec63922ab4aa2cb15a29a3be0eea0005
Pukkkkk <jidapa.petc@bumail.net>
2024-02-12 19:55:39 +0700

    Zoom Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,6 +15,16 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
 
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+
+    [SerializeField] private float dist; //between camera base and camera
+
+
     public static CameraController instance;
 
     void Awake()
@@ -27,12 +37,17 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         moveSpeed = 50;
+
+        zoomSpeed = 25;
+        minZoomDist = 15;
+        maxZoomDist = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveByKB();
+        Zoom();
     }
 
     private void MoveByKB()
@@ -53,6 +68,24 @@ public class CameraController : MonoBehaviour
                                   Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
 
         return pos;
+    }
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = 0.01f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = -0.01f;
+
+        dist = Vector3.Distance(transform.position, cam.transform.position);
+
+        if (dist < minZoomDist && zoomModifier > 0f)
+            return; //too close
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+            return; //too far
+
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
     }
 
 }
