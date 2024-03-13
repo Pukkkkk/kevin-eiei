6ab87d94980e271f3db57d65825372944a7385ef
Pukkkkk <jidapa.petc@bumail.net>
2024-03-13 09:31:19 +0700

    Finish Resource Manager

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] woodTreePrefab;
+
+    [SerializeField]
+    private Transform woodTreeParent;
+
+    [SerializeField]
+    private ResourceSource[] resources;
+
+    public static ResourceManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindAllResource();
+    }
+
+    private void FindAllResource()
+    {
+        resources = FindObjectsOfType<ResourceSource>();
+    }
+
+}
