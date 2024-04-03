cb9a35856bc2bdc616f3e480fc2e559faa276984
Pukkkkk <jidapa.petc@bumail.net>
2024-04-03 10:48:29 +0700

    Unfinish making a house

--- Assets/Scripts/AI/AIBehaviour/AICreateHQ.cs
+++ Assets/Scripts/AI/AIBehaviour/AICreateHQ.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AICreateHQ : AIBehaviour
+public abstract class AICreateHQ : AIBehaviour
 {
     protected AISupport support;
 
